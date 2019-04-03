646672e8c7c0e417b1717b6f2d1a423b60e8fe5f
mathematical-application-3607 <hannamarie.mehmet@wework.com>
2019-04-03 17:10:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
 document.addEventListener("DOMContentloaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
+
 });
+
+ 
+document.getElementById("title").innerHTML = "Titanic"
+
+document.getElementById("director").innerHTML = "James Cameron"
+
+document.getElementById("genre").innerHTML = "Drama"
+
+document.getElementById("filmRating").innerHTML = "PG-13"
+
+document.getElementById("poster").src = "https://curriculum-content.s3.amazonaws.com/web-development/cruise-ship.jpg"
+
+document.getElementById("audienceScore").innerHTML = "7.8" 
+
+document.getElementById("description").innerHTML = "A blooming romance is tested when an unexpected event occurs. Jack and Rose are forced to work together and escape from Billy Zane." 
+
+const castTitanic =
+  [
+  {
+  role: "Jack",
+  actor: "Leonardo DiCaprio",
+  },
+  {
+  role: "Rose",
+  actor: "Kate Winslett",
+  },
+  {
+  role: "Cal",
+  actor: "Billy Zane",
+  },
+  {
+  role: "The Titanic",
+  actor: "The Titanic",
+  }
+  ]
+  
+  document.getElementById("cast").innerHTML = castTitanic
+
+
